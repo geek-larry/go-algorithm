033e43f9ef4694af65b7850a7cfdbb1e34acee4d
larry <songyx0303@126.com>
2024-08-05 21:22:55 +0800

    [ADD] 字母异位词

---
+++ 438. 找到字符串中所有字母异位词/main.go
@@ -0,0 +1,36 @@
+package main
+
+func findAnagrams(s, p string) (ans []int) {
+	sLen, pLen := len(s), len(p)
+	if sLen < pLen {
+		return
+	}
+
+	var sCount, pCount [26]int
+	for i, ch := range p {
+		sCount[s[i]-'a']++
+		pCount[ch-'a']++
+	}
+	if sCount == pCount {
+		ans = append(ans, 0)
+	}
+
+	for i, ch := range s[:sLen-pLen] {
+		sCount[ch-'a']--
+		sCount[s[i+pLen]-'a']++
+		if sCount == pCount {
+			ans = append(ans, i+1)
+		}
+	}
+	return
+}
+
+func main() {
+	var a, b string
+	a = "abab"
+	b = "ab"
+	var ans = findAnagrams(a, b)
+	for _, v := range ans {
+		println(v)
+	}
+}
